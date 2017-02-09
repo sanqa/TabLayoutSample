543b5d3c322fb4570740ca8c693627bc5d972268
Alexander Balyas <balyas.java@gmail.com>
2017-02-09 16:23:18 +0200

    Change to SmartTabLayout

--- app/src/main/java/balyas/sasha/tablayoutsample/MainActivity.java
+++ app/src/main/java/balyas/sasha/tablayoutsample/MainActivity.java
@@ -1,74 +1,88 @@
 package balyas.sasha.tablayoutsample;
 
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import balyas.sasha.tablayoutsample.ui.SlidingTabLayout;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.ViewPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.ViewPagerItems;
+
+import balyas.sasha.tablayoutsample.ui.TintableImageView;
+
 
 public class MainActivity extends FragmentActivity {
 
-    private SlidingTabLayout tabs;
+    private SmartTabLayout tabs;
     private ViewPager pager;
-    private MyPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabs = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        tabs = (SmartTabLayout) findViewById(R.id.sliding_tabs);
         pager = (ViewPager) findViewById(R.id.pager);
-        adapter = new MyPagerAdapter(getSupportFragmentManager());
 
-        pager.setAdapter(adapter);
-        tabs.setCustomTabView(R.layout.custom_tab_title, R.id.tabtext, R.id.tabimage);
+        ViewPagerItemAdapter pagerAdapter = new ViewPagerItemAdapter(ViewPagerItems.with(this)
+                .add("first_layout", R.layout.test1)
+                .add("second_layout", R.layout.test2)
+                .add("third_layout", R.layout.test2)
+                .add("fourth_layout", R.layout.test2)
+                .create());
+        pager.setAdapter(pagerAdapter);
 
+        final LayoutInflater inflater = LayoutInflater.from(tabs.getContext());
+        final Resources res = tabs.getContext().getResources();
+
+        tabs.setCustomTabView(new SmartTabLayout.TabProvider() {
+            @Override
+            public View createTabView(ViewGroup container, int position, PagerAdapter adapter) {
+                LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.custom_tab_title, container, false);
+                ImageView icon = (ImageView) layout.findViewById(R.id.custom_tab_icon);
+                TextView text = (TextView) layout.findViewById(R.id.custom_tab_text);
+                switch (position) {
+                    case 0:
+                        icon.setImageDrawable(res.getDrawable(R.drawable.icon1));
+                        text.setText("Українська мова та література");
+                        break;
+                    case 1:
+                        icon.setImageDrawable(res.getDrawable(R.drawable.icon2));
+                        text.setText("Математика");
+                        break;
+                    case 2:
+                        icon.setImageDrawable(res.getDrawable(R.drawable.icon3));
+                        text.setText("Історія України");
+                        break;
+                    case 3:
+                        icon.setImageDrawable(res.getDrawable(R.drawable.icon4));
+                        text.setText("Фізика");
+                        break;
+                    default:
+                        throw new IllegalStateException("Invalid position: " + position);
+                }
+                return layout;
+            }
+        });
         tabs.setViewPager(pager);
 
+
     }
 
-
-
-
-
-    public class MyPagerAdapter extends FragmentPagerAdapter {
-
-        private final String[] TITLES = { "Українська мова та література", "Математика", "Географія", "Фізика" };
-        private final Drawable[] icons = {getResources().getDrawable(R.drawable.icon1),
-                getResources().getDrawable(R.drawable.icon2),
-                getResources().getDrawable(R.drawable.icon3),
-                getResources().getDrawable(R.drawable.icon4)} ;
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            return TITLES[position];
-        }
-
-        @Override
-        public int getCount() {
-            return TITLES.length;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return SuperAwesomeCardFragment.newInstance(position);
-        }
-
-        public Drawable getLogo(final int i) {
-            return icons[i];
-        }
-    }
 
 }
